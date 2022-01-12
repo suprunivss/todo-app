366ec0bb417cb40b42e473eb6e39c663d250d231
Martusin <suprunivss@gmail.com>
2022-01-12 16:19:08 +0300

    feat: Add create and remove functional

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles/App.css';
+import PostList from './components/PostList';
+import PostForm from './components/PostForm';
 
 function App() {
+  const [items, setItems] = useState([
+    { id: 1, title: 'Купить гвозди'},
+    { id: 2, title: 'Купить молоток'},
+  ]);
+
+  const createPost = (newPost) => {
+    setItems([...items, newPost])
+  }
+
+  const removePost = (post) => {
+    setItems(items.filter(item => item.id !== post.id))
+  }
+
   return (
-    <div className="App">
+    <div className="App container">
+      <PostForm create={createPost}/>
+      {items.length !== 0
+        ? <PostList remove={removePost} items={items}/>
+        : <h2 style={{textAlign: 'center', margin: 30, fontSize: 30}}>Todo List Empty</h2>
+      }
     </div>
   );
 }
